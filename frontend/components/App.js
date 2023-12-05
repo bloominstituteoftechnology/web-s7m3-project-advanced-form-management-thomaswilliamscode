dbcb4e8bbf1bf32728a79e5e880beaaed64481db
thomaswilliamscode <64794727+thomaswilliamscode@users.noreply.github.com>
2023-12-05 18:01:38 -0500

    Initial Commit

--- frontend/components/App.js
+++ frontend/components/App.js
@@ -1,6 +1,10 @@
 // ❗ The ✨ TASKS inside this component are NOT IN ORDER.
 // ❗ Check the README for the appropriate sequence to follow.
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
+import * as yup from 'yup'
+
+const endpoint = 'https://webapis.bloomtechdev.com/registration';
 
 const e = { // This is a dictionary of validation error messages.
   // username
@@ -21,11 +25,25 @@ const e = { // This is a dictionary of validation error messages.
 // ✨ TASK: BUILD YOUR FORM SCHEMA HERE
 // The schema should use the error messages contained in the object above.
 
+const initialForm = {
+  name: '',
+  favLanguage: '',
+  favFood: '',
+  agree: false,
+}
+
 export default function App() {
   // ✨ TASK: BUILD YOUR STATES HERE
-  // You will need states to track (1) the form, (2) the validation errors,
-  // (3) whether submit is disabled, (4) the success message from the server,
+  // You will need states to track (1) the form, 
+  const [form, setForm] = useState(initialForm)
+  // (2) the validation errors,
+  const [errors, setErrors] = useState(null)
+  // (3) whether submit is disabled, 
+  const [canSubmit, setToSubmit] = useState(form.agree)
+  // (4) the success message from the server,
+  const [serverSuccess, setServerSuccess] = useState('')
   // and (5) the failure message from the server.
+  const [serverFailure, setServerFailure] = useState('');
 
   // ✨ TASK: BUILD YOUR EFFECT HERE
   // Whenever the state of the form changes, validate it against the schema
@@ -40,24 +58,50 @@ export default function App() {
   }
 
   const onSubmit = evt => {
-    // ✨ TASK: IMPLEMENT YOUR SUBMIT HANDLER
-    // Lots to do here! Prevent default behavior, disable the form to avoid
-    // double submits, and POST the form data to the endpoint. On success, reset
-    // the form. You must put the success and failure messages from the server
-    // in the states you have reserved for them, and the form
-    // should be re-enabled.
+		// ✨ TASK: IMPLEMENT YOUR SUBMIT HANDLER
+		// Lots to do here! Prevent default behavior,
+		evt.preventDefault();
+		// disable the form to avoid double submits,
+		setToSubmit(form.agree);
+		// and POST the form data to the endpoint. 
+    axios.post(endpoint, form)
+      .then(res => {
+        setServerSuccess(res.message)
+        setServerFailure('')
+        setForm(initialForm)
+      })
+      .catch(err => {
+        setServerFailure(err.message)
+        setServerSuccess('')
+      })
+    // On success, reset the form. 
+    // You must put the success and failure messages from the server
+		// in the states you have reserved for them, and the form
+		// should be re-enabled.
+	}
+
+  function DisplayMessage(){
+    if (!serverSuccess && !serverFailure) {
+      return ''
+    } else if (serverSuccess) {
+      return <h4 className='success'>{serverSuccess}</h4>;
+    } else if (serverFailure) {
+      return <h4 className='error'>{serverFailure}</h4>;
+    }
   }
 
   return (
     <div> {/* TASK: COMPLETE THE JSX */}
       <h2>Create an Account</h2>
-      <form>
-        <h4 className="success">Success! Welcome, new user!</h4>
-        <h4 className="error">Sorry! Username is taken</h4>
-
+      <form onSubmit={onSubmit}>
+        <DisplayMessage />
         <div className="inputGroup">
           <label htmlFor="username">Username:</label>
-          <input id="username" name="username" type="text" placeholder="Type Username" />
+          <input 
+            id="username" 
+            name="username" 
+            type="text" 
+            placeholder="Type Username" />
           <div className="validation">username is required</div>
         </div>
 
@@ -65,11 +109,17 @@ export default function App() {
           <fieldset>
             <legend>Favorite Language:</legend>
             <label>
-              <input type="radio" name="favLanguage" value="javascript" />
+              <input 
+                type="radio" 
+                name="favLanguage" 
+                value="javascript" />
               JavaScript
             </label>
             <label>
-              <input type="radio" name="favLanguage" value="rust" />
+              <input 
+                type="radio" 
+                name="favLanguage" 
+                value="rust" />
               Rust
             </label>
           </fieldset>
@@ -78,7 +128,9 @@ export default function App() {
 
         <div className="inputGroup">
           <label htmlFor="favFood">Favorite Food:</label>
-          <select id="favFood" name="favFood">
+          <select 
+            id="favFood" 
+            name="favFood">
             <option value="">-- Select Favorite Food --</option>
             <option value="pizza">Pizza</option>
             <option value="spaghetti">Spaghetti</option>
@@ -89,14 +141,19 @@ export default function App() {
 
         <div className="inputGroup">
           <label>
-            <input id="agreement" type="checkbox" name="agreement" />
+            <input 
+              id="agreement" 
+              type="checkbox" 
+              name="agreement" />
             Agree to our terms
           </label>
           <div className="validation">agreement is required</div>
         </div>
 
         <div>
-          <input type="submit" disabled={false} />
+          <input 
+            type="submit" 
+            disabled={!canSubmit} />
         </div>
       </form>
     </div>
